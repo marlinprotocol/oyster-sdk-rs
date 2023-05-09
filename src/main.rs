dfb3b8b463c01f93e85dbdcecfe0b9973bd8e2b3
Roshan Raghupathy <thisisroshansmail@gmail.com>
2023-05-09 10:01:23 +0530

    lint

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use clap::Parser;
+use oyster::{get_attestation_doc, verify};
 use std::error::Error;
 use std::fs::File;
 use std::io::Write;
 use tokio;
-use oyster::{get_attestation_doc, verify};
-
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -49,7 +48,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let pcrs = vec![cli.pcr0, cli.pcr1, cli.pcr2];
     let attestation_doc = get_attestation_doc(cli.endpoint.parse()?).await?;
 
-    let pub_key = verify(attestation_doc, pcrs, cli.min_cpus, cli.min_mem, cli.max_age)?;
+    let pub_key = verify(
+        attestation_doc,
+        pcrs,
+        cli.min_cpus,
+        cli.min_mem,
+        cli.max_age,
+    )?;
     println!("verification successful with pubkey: {:?}", pub_key);
 
     let mut file = File::create(cli.public)?;
@@ -57,4 +62,3 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
-
