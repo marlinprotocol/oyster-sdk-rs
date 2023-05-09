43265053331de7d2294221778b6a9b88010b28d1
Roshan Raghupathy <thisisroshansmail@gmail.com>
2023-05-09 10:04:19 +0530

    lint

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,29 @@
 use aws_nitro_enclaves_cose::{crypto::Openssl, CoseSign1};
+use chrono::Utc;
 use hex;
+use hyper::{client::Client, Uri};
 use openssl::asn1::Asn1Time;
 use openssl::x509::{X509VerifyResult, X509};
+use serde::Deserialize;
 use serde_cbor::{self, value, value::Value};
+use serde_json;
 use std::collections::BTreeMap;
 use std::error::Error;
-use hyper::{client::Client, Uri};
-use serde_json;
-use serde::Deserialize;
-use chrono::Utc;
 
 
 #[derive(Deserialize)]
 struct EnclaveConfig {
     total_memory: usize,
-    total_cpus : usize
+    total_cpus: usize,
 }
 
 fn get_all_certs(cert: X509, cabundle: Vec<Value>) -> Result<Vec<X509>, Box<dyn Error>> {
     let mut all_certs = vec![cert];
     for cert in cabundle {
-        let cert = (match cert { Value::Bytes(b) => Ok(b), _ => Err("cert decode failure") })?;
+        let cert = (match cert {
+            Value::Bytes(b) => Ok(b),
+            _ => Err("cert decode failure"),
+        })?;
         let cert = X509::from_der(&cert)?;
         all_certs.push(cert);
     }
@@ -77,7 +80,10 @@ pub fn verify(
         let pcr = pcrs_arr
             .remove(&i.into())
             .ok_or(format!("pcr{i} not found"))?;
-        let pcr = (match pcr { Value::Bytes(b) => Ok(b), _ => Err("pcr decode failure") })?;
+        let pcr = (match pcr {
+            Value::Bytes(b) => Ok(b),
+            _ => Err("pcr decode failure"),
+        })?;
         if hex::encode(pcr) != pcrs[i as usize] {
             return Err(format!("pcr{i} match failed").into());
         }
@@ -87,7 +93,10 @@ pub fn verify(
     let enclave_certificate = attestation_doc
         .remove(&"certificate".to_owned().into())
         .ok_or("certificate key not found in attestation doc".to_owned())?;
-    let enclave_certificate = (match enclave_certificate { Value::Bytes(b) => Ok(b), _ => Err("enclave certificate decode failure") })?;
+    let enclave_certificate = (match enclave_certificate {
+        Value::Bytes(b) => Ok(b),
+        _ => Err("enclave certificate decode failure"),
+    })?;
     let enclave_certificate = X509::from_der(&enclave_certificate)?;
     let pub_key = enclave_certificate.public_key()?;
     let verify_result = cosesign1.verify_signature::<Openssl>(&pub_key)?;
@@ -100,7 +109,10 @@ pub fn verify(
     let cabundle = attestation_doc
         .remove(&"cabundle".to_owned().into())
         .ok_or("cabundle key not found in attestation doc".to_owned())?;
-    let mut cabundle = (match cabundle { Value::Array(b) => Ok(b), _ => Err("cabundle decode failure") })?;
+    let mut cabundle = (match cabundle {
+        Value::Array(b) => Ok(b),
+        _ => Err("cabundle decode failure"),
+    })?;
     cabundle.reverse();
 
     let root_cert_pem = include_bytes!("./aws.cert").to_vec();
@@ -110,7 +122,10 @@ pub fn verify(
     let user_data = attestation_doc
         .remove(&"user_data".to_owned().into())
         .ok_or("user data not found in attestation doc".to_owned())?;
-    let user_data = (match user_data { Value::Bytes(b) => Ok(b), _ => Err("user data decode failure") })?;
+    let user_data = (match user_data {
+        Value::Bytes(b) => Ok(b),
+        _ => Err("user data decode failure"),
+    })?;
     let size = serde_json::from_slice::<EnclaveConfig>(user_data.as_slice())?;
     if size.total_cpus < min_cpus {
         return Err("enclave does not meet minimum cpus requirement".into());
@@ -123,7 +138,10 @@ pub fn verify(
     let timestamp = attestation_doc
         .remove(&"timestamp".to_owned().into())
         .ok_or("timestamp not found in attestation doc".to_owned())?;
-    let timestamp = (match timestamp { Value::Integer(b) => Ok(b), _ => Err("timestamp decode failure") })?;
+    let timestamp = (match timestamp {
+        Value::Integer(b) => Ok(b),
+        _ => Err("timestamp decode failure"),
+    })?;
     let now = Utc::now().timestamp_millis();
     println!("{}, {}, {}", (now as i128), (max_age as i128), timestamp);
     if (now as i128) - (max_age as i128) > timestamp {
@@ -134,7 +152,10 @@ pub fn verify(
     let public_key = attestation_doc
         .remove(&"public_key".to_owned().into())
         .ok_or("public key not found in attestation doc".to_owned())?;
-    let public_key = (match public_key { Value::Bytes(b) => Ok(b), _ => Err("public key decode failure") })?;
+    let public_key = (match public_key {
+        Value::Bytes(b) => Ok(b),
+        _ => Err("public key decode failure"),
+    })?;
 
     Ok(public_key[0..32].try_into()?)
 }
@@ -144,4 +165,3 @@ pub async fn get_attestation_doc(endpoint: Uri) -> Result<Vec<u8>, Box<dyn Error
     let res = client.get(endpoint).await?;
     Ok(hyper::body::to_bytes(res).await?.to_vec())
 }
-
